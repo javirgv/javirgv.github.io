bebe7958523d4edbcc84a13d1f8cb5193cb9a5e5
javirgv <145112244+javirgv@users.noreply.github.com>
2026-04-29 14:34:08 +0000

    Deploying to gh-pages from @ javirgv/javirgv.github.io@f8d3241e2225107fa175d02450185e2c23e8eb66 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "A summary of my education, research experience, technical skills, projects, teaching, leadership, awards, and selected activities.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
